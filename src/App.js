6da112a6df0b5f012d54690aaea264736754deed
JangYoungRok <jyr@gateplus.org>
2025-05-23 16:54:21 +0900

    ch03-06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+// import React from "react";
+import { useState } from "react";
 import './App.css';
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             expenses: [
+//                 {id: 1, charge: "렌트비", amount: 1600},
+//                 {id: 2, charge: "교통비", amount: 400},
+//                 {id: 3, charge: "식비", amount: 1200}
+//             ]
+//         }
+//     }
+//
+//     // initialExpenses = [
+//     //   {id: 1, charge: "렌트비", amount: 1600},
+//     //   {id: 2, charge: "교통비", amount: 400},
+//     //   {id: 3, charge: "식비", amount: 1200}
+//     // ]
+//
+//     handleDelete = (id) => {
+//         // console.log(id)
+//         // const newExpenses = this.state.expenses.filter(expense => expense.id !== id)
+//         // console.log(newExpenses)
+//         // this.setState({expenses: this.state.expenses.filter(expense => expense.id !== id)})
+//         const newExpenses = this.state.expenses.filter(expense => expense.id !== id)
+//         this.setState({expenses: newExpenses})
+//     }
+//
+//     render()  {
+//         return (
+//                 <main className="main-container">
+//                     <h1>예산 계산기</h1>
+//
+//                     <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+//                         <ExpenseForm />
+//                     </div>
+//
+//                     <div style={{width:'100%', backgroundColor: 'white', padding: '1rem'}}>
+//                         {/* Props 사용 */}
+//                         <ExpenseList initialExpenses={this.state.expenses}
+//                                      handleDelete={this.handleDelete}/>
+//                     </div>
+//
+//                     <div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
+//                         <p style={{ fontSize: '2rem'}}>
+//                             총지출:
+//                             <span>원</span>
+//                         </p>
+//                     </div>
+//                 </main>
+//         )
+//     }
+// }
+
+const App = () => {
+
+    const [charge, setCharge] = useState("");
+    const [amount, setAmount] = useState(0);
+
+    const handleCharge = (e) => {
+        setCharge(e.target.value)
+    }
+
+    const handleAmount = (e) => {
+        setAmount(e.target.valueAsNumber)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (charge !== "" && amount > 0) {
+            const newExpense = {id: crypto.randomUUID(), charge, amount};
+            const newExpenses = [...expenses, newExpense];
+            setExpenses(newExpenses);
+            setCharge("");
+            setAmount(0);
+        }
+    }
+
+    const [expenses, setExpenses] = useState([
+        {id: 1, charge: "렌트비", amount: 1600},
+        {id: 2, charge: "교통비", amount: 400},
+        {id: 3, charge: "식비", amount: 1200}
+    ])
+
+    const handleDelete = (id) => {
+        const newExpenses = expenses.filter(expense => expense.id !== id)
+        setExpenses(newExpenses)
+    }
+
+    return (
+            <main className="main-container">
+                <h1>예산 계산기</h1>
+
+                <div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+                    <ExpenseForm
+                            handleCharge={handleCharge}
+                            charge={charge}
+                            handleAmount={handleAmount}
+                            amount={amount}
+                            handleSubmit={handleSubmit}
+                    />
+                </div>
+
+                <div style={{width:'100%', backgroundColor: 'white', padding: '1rem'}}>
+                    {/* Props 사용 */}
+                    <ExpenseList initialExpenses={expenses}
+                                 handleDelete={handleDelete}/>
+                </div>
+
+                <div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
+                    <p style={{ fontSize: '2rem'}}>
+                        총지출:
+                        <span>원</span>
+                    </p>
+                </div>
+            </main>
+    )
 }
 
 export default App;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,32 +1,52 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 import './ExpenseForm.css';
 import ExpenseItem from "./ExpenseItem";
 import {MdDelete} from "react-icons/md";
-import {ReactComponent} from "*.svg";
 
-class ExpenseList extends Component {
 
-    render() {
-        // console.log(this.props.initialExpense);
+// class ExpenseList extends Component {
+//
+//     render() {
+//         // console.log(this.props.initialExpense);
+//
+//         return (
+//                 <>
+//                     <ul className='list'>
+//                         {/* Expense Item */}
+//                         {this.props.initialExpenses.map(expense =>{
+//                             return (
+//                                     <ExpenseItem expense={expense} key={expense.id}
+//                                                  handleDelete={this.props.handleDelete}/>
+//                             )
+//                         })}
+//                     </ul>
+//                     <button className='btn'>
+//                         목록 지우기
+//                         <MdDelete className='btn-icon'/>
+//                     </button>
+//                 </>
+//         )
+//     }
+// }
 
-        return (
+const ExpenseList = ({ handleDelete, initialExpenses }) => {
+    return (
             <>
                 <ul className='list'>
                     {/* Expense Item */}
-                    {this.props.initialExpense.map(expense =>{
+                    {initialExpenses.map(expense =>{
                         return (
-                            <ExpenseItem expense={expense} key={expense.id}/>
+                                <ExpenseItem expense={expense} key={expense.id}
+                                             handleDelete={handleDelete}/>
                         )
                     })}
-                    {/*<ExpenseItem />*/}
                 </ul>
                 <button className='btn'>
                     목록 지우기
                     <MdDelete className='btn-icon'/>
                 </button>
             </>
-        )
-    }
+    )
 }
 
 export default ExpenseList;
